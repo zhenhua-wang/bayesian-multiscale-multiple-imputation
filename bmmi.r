41379a93c06a64147cf296a1824d650d478c9108
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2023-06-07 18:11:37 -0500

    update

--- bmmi.r
+++ bmmi.r
@@ -78,12 +78,12 @@ bmmi <- function(num_iter, num_burning, y, y_agg, miss, miss_agg,
       ## sample xi
       alpha_star_j <- alpha[j] + (N - 1)/2
       beta_star_j <- beta[j] +
-        sum(0.5 * (theta[j, -1] - theta[j, -N])^2 / sigma2[j])
+        0.5 * sum((theta[j, -1] - theta[j, -N])^2 / sigma2[j])
       xi[j] <- 1/rgamma(1, alpha_star_j, beta_star_j)
       ## sample sigma2
       tau_star_j <- tau[j] + (2*N - 1)/2
-      kappa_star_j <- kappa[j] + sum((y[j, ] - theta[j, ])^2) +
-        sum(0.5 * (theta[j, -1] - theta[j, -N])^2 / xi[j])
+      kappa_star_j <- kappa[j] + 0.5 *sum((y[j, ] - theta[j, ])^2) +
+        0.5 * sum((theta[j, -1] - theta[j, -N])^2 / xi[j])
       sigma2[j] <- 1/rgamma(1, tau_star_j, kappa_star_j)
     }
 
